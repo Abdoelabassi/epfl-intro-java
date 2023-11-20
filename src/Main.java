3a546bc737126ee0c958616238de5be3aa2c3b35
Abdoelabassi <abderrezaq@outlook.com>
2023-11-20 23:50:18 +0100

    week1 assignemnts...

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,14 @@ public class Main {
     public static void main(String[] args) {
         // Press Opt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        final int speed_of_light = 3000000;
-        Scanner keyb = new Scanner(System.in);
-        System.out.println("enter a value for n: ");
-        int n = keyb.nextInt();
-        System.out.println("multiplying n by 5, results in " + n * 5);
+        int a = 18;
+        int b = 4;
+        int c = 1;
+
+        a /= 3;
+        c += 10;
+        b = c - a;
+
+        System.out.print(b);
     }
 }
