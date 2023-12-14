a81d7a9a0bbbf56e14ec1f5e4cd18dc01419f2e0
Abdoelabassi <abderrezaq@outlook.com>
2023-12-14 22:40:57 +0100

    math expressions using java

--- src/week2/Formules.java
+++ src/week2/Formules.java
@@ -8,21 +8,52 @@ public class Formules {
 
         double x = 0.0;
 
-        System.out.println("Entrez un variable reelle: ");
-        scanner.nextDouble();
-
-        if (x != 0)
+        System.out.print("Entrez un variable reelle: ");
+        x = scanner.nextDouble();
+        double resultat = 0.0;
+        // Expression I:  x / (1 - Math.exp(x))
+        if (x == 0)
         {
-            System.out.println("la valeur de l'expression x/1-exp(x)= " + x / (1 - Math.exp(x)));
-            System.out.println("la valuer de l'expression sqrt((sin(x) - x/20)log(sqrt(x*x - 1/x))) = " + Math.sqrt((Math.sin(x) - x/20)*Math.log(Math.sqrt(x*x - 1/x))));
-        } else if (x > 1) {
-            System.out.println("la valeur de l'expression xlog(x)exp(2/x-1) = " + x*Math.log(x)*Math.exp(2/x-1));
-        } else if (x != 2 && x != 8) {
-            System.out.println("la valeur de l'expression -x - sqrt(x*x - 8x)/2 - x = " + (-x - Math.sqrt(x*x - 8*x))/(2-x));
+            System.out.println("Expression 1 indefinie");
         }
         else
         {
-            System.out.println("expression indefinie");
+            resultat = x / (1 - Math.exp(x));
+            System.out.println("Expression 1: " + resultat);
         }
+        // Expression II:  x*Math.log(x)*Math.exp(2/x-1)
+        if (x <= 0.0 || x == 1.0)
+        {
+            System.out.println("Expression 2 indefinie");
+        }
+        else
+        {
+            resultat = x * Math.log(x) * Math.exp(2/(x - 1));
+            System.out.println("Expression 2: " + resultat);
+        }
+        // Expression III: (Math.sin(x) - x/20)*Math.log(Math.sqrt(x*x - 1/x))
+        if (x >= 0.0 && x <= 1.0)
+        {
+            System.out.println("Expression 3 indefinie");
+        }
+        else
+        {
+            resultat = (Math.sin(x) - x/20)*Math.log(Math.sqrt(x*x - 1/x));
+            if (resultat < 0.0)
+                System.out.println("indefinie");
+            else
+                System.out.println("Expression 3: " + Math.sqrt(resultat));
+        }
+        // Expression IV:  (-x - Math.sqrt(x*x - 8*x))/(2-x)
+        if ((x > 0.0) && (x < 8.0))
+        {
+            System.out.println("Expression 4 indefinie");
+        }
+        else
+        {
+            resultat = ( -x - Math.sqrt(x*x - 8*x))/ (2 - x);
+            System.out.println("Expression 4: " + resultat);
+        }
+
     }
 }
