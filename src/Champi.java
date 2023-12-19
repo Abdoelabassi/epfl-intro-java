52629cd3ccdc2a600673bc2f6fcd511d6ecd5752
Abdoelabassi <abderrezaq@outlook.com>
2023-12-19 23:40:40 +0100

    type de champignon

--- src/Champi.java
+++ src/Champi.java
@@ -9,13 +9,13 @@ public class Champi {
         boolean qst3;
         boolean qst4;
 
-        System.out.println("Pensez à  un champignon : amanite tue-mouches, pied bleu, girolle,");
+        System.out.println("Pensez à  un champignon : amanite tue mouches, pied bleu, girolle,");
         System.out.println("cèpe de Bordeaux, coprin chevelu ou agaric jaunissant.");
-        System.out.print("Est-ce que votre champignon a des lamelles (true : oui, false : non) ? ");
+        System.out.print("Est-ce que votre champignon a un chapeau convexe (true : oui, false : non) ? ");
         qst1 = clavier.nextBoolean();
         if (qst1 == false)
         {
-            System.out.print("Est - ce que votre champignon a des lamelles(true : oui, false : non) ?");
+            System.out.print("Est - ce que votre champignon a des lamelles (true : oui, false : non) ?");
             qst2 = clavier.nextBoolean();
             if (qst2 == false)
             {
--- src/week2/Velo.java
+++ src/week2/Velo.java
@@ -4,9 +4,9 @@ public class Velo {
 
     public static void main(String[] args) {
         Scanner clavier = new Scanner(System.in);
-        System.out.print("Donnez l’heure de début de la location (un entier) : ");
+        System.out.print("Donnez l'heure de début de la location (un entier) : ");
         int debut = clavier.nextInt();
-        System.out.print("Donnez l’heure de fin de la location (un entier) : ");
+        System.out.print("Donnez l'heure de fin de la location (un entier) : ");
         int fin = clavier.nextInt();
 
         /*******************************************
@@ -14,7 +14,7 @@ public class Velo {
          *******************************************/
 
         double prix = 0.0;
-        if ((debut < 0 && debut > 24) || (fin < 0 && fin > 24)) {
+        if (debut < 0 || fin > 24 || debut > 24 || fin < 0) {
             System.out.println("Les heures doivent être comprises entre 0 et 24 !");
         }
         else {
@@ -39,29 +39,33 @@ public class Velo {
                 }
                 else
                 {
-                    if ((debut >= 0 && debut <= 7) && (fin >= 7 && fin <= 17))
+                    if ((debut >= 0 && debut < 7) && (fin >= 7 && fin <= 17))
                     {
                         System.out.println(7 - debut + " heure(s) au tarif horaire de 1.0 franc(s)");
                         prix += (7 - debut);
                         System.out.println(fin - 7 + " heure(s) au tarif horaire de 2.0 franc(s)");
                         prix += 2 * (fin - 7);
                     }
-                    else if ((debut >= 7 && debut <= 17) && (fin >= 17 && fin <= 24)) {
+                    else if ((debut >= 7 && debut <= 17) && (fin > 17 && fin <= 24)) {
                         System.out.println( fin - 17 + " heure(s) au tarif horaire de 1.0 franc(s)");
                         prix += (fin - 17);
                         System.out.println(17 - debut + " heure(s) au tarif horaire de 2.0 franc(s)");
                         prix += 2 * (17 - debut);
                     }
-                    else if ((debut >= 0 && debut <= 7) && (fin >= 17 && fin <= 24)) {
+                    else if ((debut >= 0 && debut <= 7) && (fin > 17 && fin <= 24)) {
                         System.out.println((7 - debut) + (fin - 17) + " heure(s) au tarif horaire de 1.0 franc(s)");
                         prix += (7 - debut) + (fin - 17);
                         System.out.println("10 heure(s) au tarif horaire de 2.0 franc(s)");
                         prix += 2 * 10;
+                    } else if ((debut >= 7 && debut <= 17) && (fin > debut && fin <= 17)) {
+                        System.out.println(fin - debut + " heure(s) au tarif horaire de 2.0 franc(s)");
+                        prix += 2 * (fin - debut);
+
                     }
                 }
+                System.out.println("Le montant total à payer est de " +  prix + " franc(s).");
             }
         }
-                    System.out.println("Le montant total à payer est de " +  prix + " franc(s)");
 
         /*******************************************
          * Ne rien modifier apres cette ligne.
