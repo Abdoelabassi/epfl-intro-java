24b3d14ae9a76605d8a9c3ec366e6c9d03acf989
Abdoelabassi <abderrezaq@outlook.com>
2024-01-09 18:16:01 +0100

    parachutiste

--- src/Parachutiste.java
+++ src/Parachutiste.java
@@ -21,6 +21,36 @@ class Parachutiste {
         /*******************************************
          * Completez le programme a partir d'ici.
          *******************************************/
+        double g = 9.81;
+        double v_0 = 0.0;
+        double t_0 = 0.0;
+        double  surface_parachutiste = 2.0;
+        double s = surface_parachutiste / masse;
+        double hauteur = h0;
+        double v = v_0;
+        double a = g;
+        double t = t_0;
+        //System.out.printf("%.0f, %.4f, %.4f, %.5f\n", t, hauteur, v, a);
+        while(hauteur > 0)
+        {
+            boolean is_supersonic = true;
+            boolean is_max_v = true;
+            boolean open_parachute = true;
+            if (v > 343 && is_supersonic){
+                is_supersonic = false;
+                System.out.println("## Felix depasse la vitesse du son");
+            }
+            else if (a < 0.5 && is_max_v){
+                is_max_v = false;
+                System.out.println("## Felix a atteint sa vitesse maximale");
+            }
+            System.out.printf("%.0f, %.4f, %.4f, %.5f\n", t, hauteur, v, a);
+            double q = Math.exp(-s*(t-t_0));
+            v = (1-q)*g/s + v_0*q;
+            hauteur = h0 - g/s*(t-t_0) - (1-q)*(v_0 - g/s)/s;
+            a = g - s*v;
+            t++;
+        }
         /*******************************************
          * Ne rien modifier apres cette ligne.
          *******************************************/
