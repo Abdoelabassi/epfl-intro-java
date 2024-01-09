316b84d14f1f51e2b47805308536ba23c0561cc5
Abdoelabassi <abderrezaq@outlook.com>
2024-01-09 16:48:27 +0100

    suites

--- src/Suite.java
+++ src/Suite.java
@@ -21,6 +21,22 @@ class Suite {
         /*******************************************
          * Completez le programme a partir d'ici.
          *******************************************/
+        while(debut <= fin){
+            int n = debut;
+            int n_rep = 0;
+            while(n != 0){
+                if(n%3 == 0){
+                    n += 4;
+                }else if(n%4 == 0){
+                    n /=2;
+                }else{
+                    n -= 1;
+                }
+                n_rep++;
+            }
+            System.out.println(debut + "->" + n_rep);
+            debut++;
+        }
         /*******************************************
          * Ne rien modifier apres cette ligne.
          *******************************************/
