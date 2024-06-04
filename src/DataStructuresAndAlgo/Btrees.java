831cc499965ca9e775b4670a454cc922721ebf46
Abdoelabassi <abderrezaq@outlook.com>
2024-06-04 20:03:58 +0100

    simple binary tree

--- src/DataStructuresAndAlgo/Btrees.java
+++ src/DataStructuresAndAlgo/Btrees.java
@@ -5,7 +5,7 @@ package DataStructuresAndAlgo;
  */
 public class Btrees<T> {
 
-
+    private TreeNode<T> root;
 
     private class TreeNode<T> {
         private T data;
@@ -15,6 +15,25 @@ public class Btrees<T> {
         public TreeNode(T data) {
             this.data = data;
         }
+
+    }
+    // create a binary tree: ints
+    public void createBTree(T[] args) {
+        TreeNode<T> First = new TreeNode<>(args[0]);
+        TreeNode<T> second = new TreeNode<>(args[1]);
+        TreeNode<T> third = new TreeNode<>(args[2]);
+        TreeNode<T> fourth = new TreeNode<>(args[3]);
+        TreeNode<T> fifth = new TreeNode<>(args[4]);
+        TreeNode<T> sixth = new TreeNode<>(args[5]);
+        TreeNode<T> seventh = new TreeNode<>(args[6]);
+
+        root = First; // root --> first
+        First.left = second;
+        First.right = third; // second <--- first ---> third
+        second.left = fourth;
+        second.right = fifth;
+        third.left = sixth;
+        third.right = seventh;
 
     }
 }
